b6c2df1ae02da38ff768d6819db961a8d205c68f
joelm <joelmoreraapaza@gmail.com>
2024-02-05 09:48:30 +0000

    Metodos Añadidos

--- Diccionario.java
+++ Diccionario.java
@@ -3,4 +3,25 @@ public class Diccionario {
     public Diccionario {
 
     }
+    public boolean insertar(Object obj) {
+
+    }
+
+    public boolean eliminar(Object obj) {
+
+    }
+
+    public Object recoger(Object obj) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public Object[] values() {
+
+    }
+
+
 }
