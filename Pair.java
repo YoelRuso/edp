a9b1e6f735622b8f836d020c63e808bd93b5df50
tigor7 <63234779+tigor7@users.noreply.github.com>
2024-02-12 09:06:04 +0000

    Ejamplo

---
+++ Pair.java
@@ -0,0 +1,13 @@
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+}
---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Pair<String, Integer> p = new Pair<String, Integer>("test", 3);
+        System.out.println(p.getValue());
+    }
+}
