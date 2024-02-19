8b0178778a71deffcd34576ac23978cbb58f5edf
Maríacv <informatica1bachIES@gmail.com>
2024-02-19 08:46:43 +0000

    Se añade el método update

--- Main.java
+++ Main.java
@@ -2,10 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         Pair<String, Integer> p = new Pair<String, Integer>("test", 3);
-        Dictionary dict = new Dictionary("test1", 1);
-        dict.insert("test2", 2);
-        dict.remove("test1");
-        dict.get("test1");
-        System.out.println(dict.get("test2"));
+        System.out.println(p.getValue());
     }
 }
